c50df543bf01bb9054d1e0500efc2e27271751e6
Arianna Johnson <ariannajohnson588@gmail.com>
2021-04-26 17:47:08 -0400

    second twosum

--- TwoSum.js
+++ TwoSum.js
@@ -1,7 +1,9 @@
+// O(n^2) time | O(1) space
+
 function twoNumberSum(array, targetSum) {
-    for(let i = 0; i < array.length -1; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
         let num1 = array[i];
-        for(let j= i+1; j < array.length; j++) {
+        for (let j = i + 1; j < array.length; j++) {
             let num2 = array[j];
             if ((num1 + num2) === targetSum) {
                 return [num1, num2]
@@ -9,6 +11,27 @@ function twoNumberSum(array, targetSum) {
         }
     }
     return [];
-  }
+}
 
-  console.log(twoNumberSum([2,3,5,7,9], 8))
+console.log(twoNumberSum([2, 3, 5, 7, 9], 8))
+
+// O(nlogn) time | O(1) space
+
+function twoNumberSum2(array, targetSum) {
+    array.sort((a, b) => a - b);
+    let left = 0;
+    let right = array.length-1;
+    while(left < right) {
+        const currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]];
+        } else if (currentSum < targetSum) {
+            left++;
+        } else if (currentSum > targetSum) {
+            right--;
+        }
+    }
+    return [];
+}
+
+console.log(twoNumberSum2([2, 3, 5, 7, 9], 8))
