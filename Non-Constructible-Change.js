05da55a51a67a4517c95b6eeef27b4d091301a33
Arianna Johnson <ariannajohnson588@gmail.com>
2021-06-02 16:51:19 -0400

    finished problem

--- Non-Constructible-Change.js
+++ Non-Constructible-Change.js
@@ -1,10 +1,16 @@
 function nonConstructibleChange(coins) {
     coins.sort((a, b) => a - b);
+    console.log(coins)
       let change = 0;
       coins.forEach(coin => {
+          console.log(change)
           if (coin > change +1) return change + 1;
 
           change += coin;
       })
       return change + 1;
   }
+
+
+let test1 = [5, 7, 1, 1, 2, 3, 22]
+console.log(nonConstructibleChange(test1))
