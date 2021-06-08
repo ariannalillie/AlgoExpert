3d20358c44ffd8236d54cc84dcffc947cb552810
Arianna Johnson <ariannajohnson588@gmail.com>
2021-06-07 22:28:02 -0400

    post order

--- BstTraversal.js
+++ BstTraversal.js
@@ -18,4 +18,12 @@ function preOrderTraverse(tree, array) {
 	return array;
 }
 
+function postOrderTraverse(tree, array) {
+ 	if (!tree) return;
 
+  postOrderTraverse(tree.left, array);
+	postOrderTraverse(tree.right, array)
+	array.push(tree.value)
+
+	return array;
+}
