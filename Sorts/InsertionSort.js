9bcd71acadd5c69a796fc9413458aedb288a3332
Arianna Johnson <ariannajohnson588@gmail.com>
2021-06-07 22:34:58 -0400

    insertion sort setup

---
+++ Sorts/InsertionSort.js
@@ -0,0 +1,18 @@
+function insertionSort(arr) {
+
+    // Set a pointer diving the array into sorted and unsorted halves
+
+    // Repeat while the unsorted half is not empty:
+
+      // Grab the first value from the unsorted half
+
+      // For each sorted value in the array starting from the divider,
+        // Check if the value to the left is smaller than the unsorted value
+          // If so, you've reached the insertion point so exit the loop
+          // If not shift the value to the right by 1 and continue
+
+      // Insert the unsorted value at the insertion point
+
+      // Increment the dividing pointer and repeat
+
+  }
