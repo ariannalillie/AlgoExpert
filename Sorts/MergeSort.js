5131d446183020f7dd428f438af02bf340203e46
Arianna Johnson <ariannajohnson588@gmail.com>
2021-06-08 13:27:34 -0400

    merge setup

---
+++ Sorts/MergeSort.js
@@ -0,0 +1,11 @@
+// Merge Sort is known as a divid and conquer algorithm (continuesly divids an array into two and then sorts the divided arrays)
+
+
+function easyMergeSort(array) {
+
+};
+
+
+function HardMergeSort(array) {
+
+};
