c8467d852fd796136d1ffba1fa7da2f928c95f59
Arianna Johnson <ariannajohnson588@gmail.com>
2021-06-08 11:25:53 -0400

    selection sort

--- Sorts/InsertionSort.js
+++ Sorts/InsertionSort.js
@@ -1,10 +1,12 @@
 function insertionSort(arr) {
 
     // Set a pointer diving the array into sorted and unsorted halves
-
+    let pointer = 0;
+    let temp;
     // Repeat while the unsorted half is not empty:
-
+    while (temp < arr.length) {
       // Grab the first value from the unsorted half
+      temp = arr[0];
 
       // For each sorted value in the array starting from the divider,
         // Check if the value to the left is smaller than the unsorted value
@@ -14,5 +16,6 @@ function insertionSort(arr) {
       // Insert the unsorted value at the insertion point
 
       // Increment the dividing pointer and repeat
-
+    }
   }
+let arr = [3, 2, 0, 4, 1]
---
+++ Sorts/SelectionSort.js
@@ -0,0 +1,27 @@
+// time 0(n2) *nested loops*/ space 0(1)
+
+function selectionSort(array) {
+	// current index is equal to the first num in the unsorted list
+  let currIdx = 0
+	// once this condition is met there will only be one thing left in the list
+	// and that will already be sorted
+	while (currIdx < array.length - 1) {
+		// initalize smallest index to be current index, which is the first number
+		// in the unsorted array
+		let smallestIdx = currIdx;
+		for (let i = currIdx + 1; i < array.length; i++) {
+			// iterating through the unsorted sublist to find the smallest index
+			if (array[smallestIdx] > array[i]) smallestIdx = i;
+		}
+		swap(currIdx, smallestIdx, array);
+		currIdx++;
+	}
+	return array;
+};
+
+
+function swap (i, j, array) {
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
+};
