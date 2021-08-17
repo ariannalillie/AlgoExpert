a4c414245fef50d4f2af0d687c08707594edc917
Arianna Johnson <ariannajohnson588@gmail.com>
2021-08-17 16:30:20 -0400

    finished generate docs

--- GenerateDocument.js
+++ GenerateDocument.js
@@ -1,18 +1,22 @@
-
-
-function generateDocument(characters, document) {
-  let sortedCharArr = Array.from(characters).sort().join('')
-  let sortedDocArr = Array.from(document).sort().join('')
-  for (let i = 0; i < sortedDocArr.length; i++) {
-    if (sortedCharArr.indexOf(sortedDocArr[i]) === -1) {
-      return false;
+  function generateDocument(characters, document) {
+    let sortedCharArr = Array.from(characters).sort()
+    let sortedDocArr = Array.from(document).sort()
+    for (let i = 0; i < sortedDocArr.length; i++) {
+      if (sortedCharArr.indexOf(sortedDocArr[i]) === -1) {
+         return false;
+      } else {
+        let valToRemove = sortedCharArr.indexOf(sortedDocArr[i])
+        sortedCharArr.splice(valToRemove, 1);
+        console.log(sortedCharArr)
+      }
     }
-  }
-    return true;
-  }
+      return true;
+    }
 
+  let characters1 = "Bste!hetsi ogEAxpelrt x ";
+  let document1 = "AlgoExpert is the Best!";
 
-  characters1 = "Bste!hetsi ogEAxpelrt x ";
-  document1 = "AlgoExpert is the Best!";
+  let characters2 = "aheaolabbhb";
+  let document2 = "hello"
 
-  console.log(generateDocument(characters1, document1))
+  console.log(generateDocument(characters2, document2))
