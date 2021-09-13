4fc2a37c25407f64b8d30b52963c5188f4dac2c6
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-13 15:49:46 -0400

    finished firstduplicatevalue

--- FirstDuplicateValue.js
+++ FirstDuplicateValue.js
@@ -1,4 +1,20 @@
+// steps:
+// create a new set to push values into
+// iterate through the array
+    // if set already has value return value as we have found the first duplicate
+    // if not add value to the set
+//if we have iterated through the entire array and have no found a duplicate return -1
+
 function firstDuplicateValue(array) {
-    // Write your code here.
-    return [];
+    const noDuplicateSet = new Set
+    for (let i = 0; i < array.length; i++) {
+        if (noDuplicateSet.has(array[i])) {
+            return array[i];
+        }
+        noDuplicateSet.add(array[i]);
+    }
+    return -1;
   }
+
+
+  console.log(firstDuplicateValue([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]))
