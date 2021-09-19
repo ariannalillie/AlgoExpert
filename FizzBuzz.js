a2397b8350fec9147db8d0489c9961aa66e38d4b
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-19 19:58:15 -0400

    uncommited changes

---
+++ FizzBuzz.js
@@ -0,0 +1,30 @@
+const getInput = () => new Promise(resolve => {
+    let input = ''
+    process.stdin.on('data', (data) => input += data);
+    process.stdin.on('end', () => {
+        resolve(input)
+    })
+})
+
+async function fizzBuzz () {
+
+    const input = await getInput()
+    let happyFace = input.split('\n');
+    let array = (happyFace[1].split(' '))
+    array.forEach(smile => {
+        for (let i = 1; i <= smile; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log('Buzz')
+        } else if (i % 5 === 0 && i % 3 === 0) {
+            console.log('FizzBuzz')
+        } else {
+            console.log(i)
+        }
+    }
+    })
+
+}
+
+fizzBuzz()
