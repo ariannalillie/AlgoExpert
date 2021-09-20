d56dc6a720a664d59432968cddf19b3751b4c011
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-20 10:16:03 -0400

    python operators

--- python_practice.py
+++ python_practice.py
@@ -1,5 +1,27 @@
 # this is what a python comment looks like
 
+# Python variables are typically snakecase
 my_name = 'Arianna'
 
 print('Hello my name is ' + my_name)
+
+this_is_an_int = 5
+
+this_is_a_float = 7.02
+
+# Python Operators
+
+#add
+print(1+1)
+
+#subtract
+print(2-1)
+
+#multiply
+print(4*5)
+
+#divide
+print(20/5)
+
+#modulo
+print(20%5)
