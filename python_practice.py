527e73101471d3e307f866e9128aacf43d52bcdb
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-20 10:23:19 -0400

    more python

--- python_practice.py
+++ python_practice.py
@@ -9,7 +9,8 @@ this_is_an_int = 5
 
 this_is_a_float = 7.02
 
-# Python Operators
+# PYTHON OPERATORS (typically denoted with no spaces between
+# operations )
 
 #add
 print(1+1)
@@ -25,3 +26,25 @@ print(20/5)
 
 #modulo
 print(20%5)
+
+
+#find length of a string
+print(len(my_name))
+
+#escaping characters
+print('Jodi asked, "What\'s up, Sam?"')
+
+#multiline string
+print('''My instructions are very long so to make them
+more readable in the code I am putting them on
+more than one line. I can even include "quotes"
+of any kind because they won't get confused with
+the end of the string!''')
+
+#index function (similiar to indexOf())
+print("Spaghetti".index("h"))    # => 4
+
+#count function
+  #Find out how many times a substring appears in the primary string using count.
+  # It returns zero if the substring is not there.
+print("Spaghetti".count("t"))    # => 2
