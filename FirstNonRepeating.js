0409f9170534e319ebeaa36d6229b1c85669fc97
Arianna Johnson <ariannajohnson588@gmail.com>
2022-01-03 18:18:52 -0500

    non repeating

---
+++ FirstNonRepeating.js
@@ -0,0 +1,18 @@
+function firstNonRepeating(string) {
+    const arr = string.split('');
+    let first
+    let letters = Set();
+    arr.forEach(char => {
+        if (letters.has(char)) {
+
+        }
+
+    })
+
+    return arr;
+}
+
+
+const testString = "abcdcaf"
+
+console.log(firstNonRepeating(testString));
