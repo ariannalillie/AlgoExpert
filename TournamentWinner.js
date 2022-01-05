4e7476bbc343f9881b760b5d8e1797dccffce92b
Arianna Johnson <ariannajohnson588@gmail.com>
2022-01-05 11:57:01 -0500

    reset tournament winner

--- TournamentWinner.js
+++ TournamentWinner.js
@@ -1,17 +1,4 @@
 function tournamentWinner(competitions, results) {
-let newObj = {}
-let result;
-
-for (let i = 0; i < competitions.length; i++) {
-     if (!newObj[competitions[i]]) newObj[competitions[i]] = 0
-     console.log(newObj)
-     for (let i = 0; i < results.length; i++) {
-         if (results[i] === 0) {
-             
-         }
-
-    }
-}
 
 }
 
