b46c26845bc4c5deec9d5d74e7c95e90a2d319ea
Arianna Johnson <ariannajohnson588@gmail.com>
2022-01-05 11:49:10 -0500

    smallest substring containing setup

---
+++ SmallestSubstringContaining.js
@@ -0,0 +1,17 @@
+// You're given two non empty strings (one big and one small)
+// return smallest substring in big string that contains all of the small strings characters
+// substring can contain other chars
+// chars don't have to be in the same order
+// substring must contain all duplicates on small string
+
+
+function smallestSubstringContaining(bigString, smallString) {
+    // Write your code here.
+};
+
+
+
+const bigStringTest = "abcd$ef$axb$c$"
+const smallStringTest = "$$abf"
+
+console.log(smallestSubstringContaining(bigStringTest, smallStringTest));
